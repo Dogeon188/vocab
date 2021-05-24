d4d7f4890a4809382b5efb73ef90f71004dddabb
Dogeon188 <dogcatdog188@gmail.com>
2021-05-24 08:05:09 +0800

    spoiler

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const utils = require("./utils")
 
 const app = express()
 const port = 443
-const hostname = "0.0.0.0"
+const hostname = "localhost"
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs')
 require("./route").route(app)
 
 app.listen(port, hostname, () => {
-    utils.log(`App listening at https://vocab.dogeon188.repl.co`)
+    utils.log(`App listening at http://${hostname}:${port}`)
 })
 
 process.on("uncaughtException", err => {
